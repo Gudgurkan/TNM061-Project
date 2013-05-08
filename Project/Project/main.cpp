679b09e535c58749644c84d0bbb9cd5ee1a833b3
Oscar Westberg <oscar.henrik.westberg@gmail.com>
2013-05-08 14:57:21 +0200

    foljer sassans tutorial

--- Project/Project/main.cpp
+++ Project/Project/main.cpp
@@ -1,79 +1,44 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-#include <GL/glew.h>
+/* Author: Stefan Gustavson (stegu@itn.liu.se) 2013
+* This code is in the public domain.
+*/
+#include <iostream>
 #include <GL/glfw.h>
-#include <glm/glm.hpp>
 
+using namespace std;
 
-int width, height;
-GLuint vertexArrayID, vertexBufferID, indexBufferID;
+void setupViewport () {
+	int width , height ;
 
-void setupViewport(){
-
-	glfwGetWindowSize(&width, &height);
-	glViewport(0,0,width,height);
-
-	static const GLfloat vertex_array_data[] = {
-		-1.0f, -1.0f, 0.0f,	//First vertex xyz
-		1.0f, -1.0f, 0.0f,	//Second vertex
-		0.5f, 1.0f, 0.0f	//Third vertex
-	};
-
-	static const GLuint index_array_data[] = {
-		0,1,2
-	};
-	
-	glGenVertexArrays(1, &vertexArrayID); //generate 1 VAO, put resulting identifier in vertexArrayID
-	glGenBuffers(1, &vertexBufferID); //generate 1 buffer, put resulting identifier in vertexBufferID
-	glGenBuffers(1, &indexBufferID); //generate 1 buffer, put resulting identifier in indexBufferID
-
-	glBindVertexArray(vertexArrayID);//activate the vertex array object
-	
-	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID); //Activate the vertex buffer object
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_array_data), vertex_array_data, GL_STATIC_DRAW); //Present our vertex coordiantes to OpenGL
-	glEnableVertexAttribArray(0); //Enable vertex attribute array 0 to send it to the shader.
-	glVertexAttribPointer(
-		0,                  // attribute. No particular reason for 0, but must match the layout in the shader.
-		3,                  // size
-		GL_FLOAT,           // type
-		GL_FALSE,           // normalized?
-		0,                  // stride
-		(void*)0            // array buffer offset
-	);
-
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferID); //Activate the index buffer object
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(index_array_data), index_array_data, GL_STATIC_DRAW); //Present out vertex indices to OpenGL
-	
-	glBindVertexArray(0); //Deactivate the vertex array object again to be nice
+	glfwGetWindowSize ( &width , &height );
+	glViewport (0, 0, width, height); // The entire window
 }
 
-int main()
+int main () 
 {
-	GLboolean running = GL_TRUE;
-
+	GLboolean running = GL_TRUE ; // Main loop exits when this is set to GL_FALSE
 	glfwInit();
-	if(!glfwOpenWindow(256, 256, 8, 8, 8, 8, 32, 0, GLFW_WINDOW)){
-		glfwTerminate();
-		return 0;
-	}
-	printf("GL renderer:     %s\n", glGetString(GL_RENDERER));
-	glfwSwapInterval(1);
 
-	while(running)
+	if(!glfwOpenWindow (256, 256, 8, 8, 8, 8, 32, 0, GLFW_WINDOW))
 	{
-		setupViewport();
-		glClearColor(0.0f,0.0f,0.0f,1.0f);
+		glfwTerminate () ; // glfwOpenWindow failed , quit the program.
+		return 1;
+	}
+
+	glfwSwapInterval (0) ; // Do not wait for screen refresh between frames
+
+	while( running )
+	{
+		setupViewport() ;
+		glClearColor(0.3f ,0.3f ,0.3f ,0.0f) ;
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		//Rendering code
-		//Activate the vertex array object we want to draw
-		glBindVertexArray(vertexArrayID);
-		glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, (void*)0);
+		// --- Rendering code should go here ---
 
-		glfwSwapBuffers();
-		if(glfwGetKey(GLFW_KEY_ESC) || !glfwGetWindowParam(GLFW_OPENED)){
-			running = GL_FALSE;
+		glfwSwapBuffers() ;
+
+		if( glfwGetKey( GLFW_KEY_ESC ) || ! glfwGetWindowParam (GLFW_OPENED)) 
+		{
+			running = GL_FALSE ;
 		}
 	}
 
