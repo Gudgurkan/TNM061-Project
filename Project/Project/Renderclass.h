a8ba5cb920b8be033849547c43777c7919ecbb17
teodorvik <teovi698@student.liu.se>
2013-05-15 15:19:52 +0200

    Renderclass av Teodor

---
+++ Project/Project/Renderclass.h
@@ -0,0 +1,16 @@
+#ifndef RENDERCLASS_H
+#define RENDERCLASS_H
+
+class Renderclass
+{
+	public:
+		//Renderclass(); //Den gillar in constructorerna :S
+		//~Renderclass();
+		
+		void RenderObject(GLuint vbuffer, GLuint cbuffer);
+		void brutalBuffer(GLuint vbuffer, GLuint cbuffer);
+	private:
+
+};
+
+#endif
---
+++ Project/Project/renderclass.cpp
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+using namespace std;
+
+#include <stdlib.h>
+#include <string.h>
+#include <GL/glew.h>
+#include "Renderclass.h"
+#include "Cube.h"
+
+
+void Renderclass::RenderObject(GLuint vertexbuffer, GLuint colorbuffer)
+{
+	// 1st attribute buffer
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	glVertexAttribPointer(  //Contains data from the previously provided pointer
+		0,                  // attribute. No particular reason for 0, but must match the layout in the shader.
+		3,                  // size
+		GL_FLOAT,           // type
+		GL_FALSE,           // normalized?
+		0,                  // stride
+		(void*)0            // array buffer offset
+	);
+
+	// 2nd attribute buffer : colors
+	glEnableVertexAttribArray(1);
+	glBindBuffer(GL_ARRAY_BUFFER, colorbuffer); //Colorbuffer has been bound to the buffer earlier
+	glVertexAttribPointer(
+		1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
+		3,                                // size
+		GL_FLOAT,                         // type
+		GL_FALSE,                         // normalized?
+		0,                                // stride
+		(void*)0                          // array buffer offset
+	);
+
+	//DRAW
+	glDrawArrays(GL_TRIANGLES, 0, 12*3); // 12*3 indices starting at 0 -> 12 triangles
+
+	glDisableVertexAttribArray(0);
+	glDisableVertexAttribArray(1);	
+}
+	
+void Renderclass::brutalBuffer(GLuint vertexbuffer, GLuint colorbuffer)
+{
+	//Buffer for vertices
+	glGenBuffers(1, &vertexbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+	//Buffer for colors at every vertex
+	glGenBuffers(1, &colorbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
+}
