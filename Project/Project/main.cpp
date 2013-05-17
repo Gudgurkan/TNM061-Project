328738f2842b4e417eaa49e48cc36dfa0993a7cd
Oscar Westberg <oscar.henrik.westberg@gmail.com>
2013-05-17 16:38:19 +0200

    Stadade upp i main

--- Project/Project/main.cpp
+++ Project/Project/main.cpp
@@ -16,10 +16,6 @@
 
 using namespace glm;
 
-//void renderB(Object obj);
-//void deleteB(Object obj);
-//
-
 void renderB(Object &obj)
 {
 	obj.RenderObject();
@@ -32,6 +28,10 @@ void deleteB(Object &obj)
 
 int main () 
 {
+	// -----------------------------------------
+	// Initialize program
+	// -----------------------------------------
+
 	//Initialize GLFW
 	if( !glfwInit() )
 	{
@@ -45,7 +45,7 @@ int main ()
 	glfwOpenWindowHint(GLFW_OPENGL_VERSION_MINOR, 3);
 	glfwOpenWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	//create an opengl window
+	// Create an opengl window
 	if(!glfwOpenWindow (1024, 768, 8, 8, 8, 8, 32, 0, GLFW_WINDOW))
 	{
 		fprintf( stderr, "Failed to open GLFW window.");
@@ -53,52 +53,60 @@ int main ()
 		return 1;
 	}
 
-	//initialize GLEW
+	// Initialize GLEW
 	glewExperimental = true;
 	if (glewInit() != GLEW_OK) {
 		fprintf(stderr, "Failed to initialize GLEW\n");
 		return -1;
 	}
-
-	//Set parametres for program
+	
+	// -----------------------------------------
+	// Set parametres for program
+	// -----------------------------------------
+	
 	glfwSetWindowTitle( "Lightning Demo" );
 	glfwEnable( GLFW_STICKY_KEYS );
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 	glEnable(GL_DEPTH_TEST);
 	glDepthFunc(GL_LESS); 
 
-	GLuint programID = LoadShaders( "TransformVertexShader.vertexshader", "ColorFragmentShader.fragmentshader" ); // Create and compile our GLSL program from the shaders
+	// Create and compile our GLSL program from the shaders
+	GLuint programID = LoadShaders( "TransformVertexShader.vertexshader", "ColorFragmentShader.fragmentshader" ); 
 	GLuint MatrixID = glGetUniformLocation(programID, "MVP");
 	GLuint ViewMatrixID = glGetUniformLocation(programID, "V");
 	GLuint ModelMatrixID = glGetUniformLocation(programID, "M");
 
+	// -----------------------------------------
+	// Add objects
+	// -----------------------------------------
+
 	std::vector<Object> objects;
 
-	//Add first object
+	// Add first object
 	Object cylinder("cylinder.obj");	
 	cylinder.translateObject(-6.0, -1.0, 0.0);
 	cylinder.scaleObject(1.0, 5.0, 1.0);
 	cylinder.BindBuffers();
 
-	//Add second cylinder object
+	// Add second cylinder object
 	Object cylinder2("cylinder.obj");	
 	cylinder2.translateObject(-2.0, -1.0, 0.0);
 	cylinder2.scaleObject(1.0, 5.0, 1.0);
 	cylinder2.BindBuffers();
 
-	//Add third cylinder object
+	// Add third cylinder object
 	Object cylinder3("cylinder.obj");
 	cylinder3.translateObject(2.0, -1.0, 0.0);
 	cylinder3.scaleObject(1.0, 5.0, 1.0);
 	cylinder3.BindBuffers();
 
-	//Add second object
+	// Add second object
 	Object sphere("sphere.obj");
 	sphere.scaleObject(0.15f, 0.15f, 0.15f); 
 	sphere.translateObject(0.0f, 0.0f, 0.0f);
 	sphere.BindBuffers();
 
-	//Add floor
+	// Add floor
 	Object floor("floor.obj");
 	floor.BindBuffers();
 
@@ -108,25 +116,41 @@ int main ()
 	//objects.push_back(cube);
 	objects.push_back(floor);
 
+	// -----------------------------------------
+	// Handle lights
+	// -----------------------------------------
+
 	// Get a handle for our "LightPosition" uniform
 	glUseProgram(programID);
 	GLuint LightID = glGetUniformLocation(programID, "LightPosition_worldspace");
 	GLuint LightID2 = glGetUniformLocation(programID, "LightPosition_worldspace2");
 	
-	//Color position
+	// Color position
 	vec3 lightPos2 = vec3(3.0,1.0,2.0);
 	glUniform3f(LightID2, lightPos2.x, lightPos2.y, lightPos2.z);
+
+	// -----------------------------------------
+	// Main loop
+	// -----------------------------------------
 	
 	do{	
-		
+		// -----------------------------------------
+		// Update program
+		// -----------------------------------------
+
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear the screen
-		glUseProgram(programID); // Use our shader
+		glUseProgram(programID);							// Use our shader
 
 		// The sin and cos-terms make the lightsource feel ALIVE! 
 		glm::vec3 lightPos = computeLightFromInputs() * addCircularMotion();
 		glUniform3f(LightID, lightPos.x, lightPos.y, lightPos.z);
 
 		updateMatrices();
+
+		// -----------------------------------------
+		// Handle view transformation matrices
+		// -----------------------------------------
+
 		mat4 Projection = getProjectionMatrix();
 		mat4 View = getViewMatrix();
 		mat4 Model      = mat4(1.0f); // Model matrix : an identity matrix (model will be at the origin)
@@ -137,31 +161,39 @@ int main ()
 		glUniformMatrix4fv(ModelMatrixID, 1, GL_FALSE, &Model[0][0]);
 		glUniformMatrix4fv(ViewMatrixID, 1, GL_FALSE, &View[0][0]);
 
+		// Render objects
 		for_each(objects.begin(), objects.end(), renderB);
+
+		// -----------------------------------------
+		// Handle light transformation
+		// -----------------------------------------
 
 		mat4 Model2 = translate(mat4(1.0f), lightPos);
 		//mat4 Model      = mat4(1.0f); // Model matrix : an identity matrix (model will be at the origin)
 		mat4 MVP2 = Projection * View * Model2; // Our ModelViewProjection : multiplication of our 3 matrices
 
-		// Send our transformation to the currently bound shader, in the "MVP" uniform
+		// Send our transformation to the currently bound shader, in the "MVP2" uniform
 		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP2[0][0]); 
 		glUniformMatrix4fv(ModelMatrixID, 1, GL_FALSE, &Model2[0][0]);
 		glUniformMatrix4fv(ViewMatrixID, 1, GL_FALSE, &View[0][0]);
 
 		sphere.RenderObject();
 
+		// -----------------------------------------
+		// Swap buffers, used for double buffering. Very nice.
+		// -----------------------------------------
 
-		glfwSwapBuffers(); // Swap buffers, used for double buffering. Very nice.
+		glfwSwapBuffers();
 
 	} // Check if the ESC key was pressed or the window was closed
 	while( glfwGetKey( GLFW_KEY_ESC ) != GLFW_PRESS && glfwGetWindowParam( GLFW_OPENED ) );
 
-	// Cleanup VBO and shader
+	// -----------------------------------------
+	// Cleanup
+	// -----------------------------------------
+
 	for_each(objects.begin(), objects.end(), deleteB);
 	glDeleteProgram(programID);
-	//glDeleteVertexArrays(1, &VertexArrayID);
-
-	// Close OpenGL window and terminate GLFW
 	glfwTerminate();
 
 	return 0;
